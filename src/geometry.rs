e59905cf52848480ee3f0ee00c10299d93e906ac
r41ngee <r41ngee@yandex.ru>
2026-07-30 06:25:40 +0300

    feat(node): enhance Node structure with additional methods for manipulation and navigation

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,18 +1,31 @@
 use crate::*;
 
+/// Enum describing a direction of [Node::Split]
 pub enum Direction {
     Row,
     Column,
 }
 
+/// Recursive enum used to build a tree
+/// of [Category] containing rectangles.
+/// 
+/// Owns a Category object or a [Vec] of child Nodes
+/// respectively.
 pub enum Node {
+    /// Terminal variant.
+    /// Containing final object of tree.
     Category(Category),
+    /// Mid variant.
     Split(Vec<Node>, Direction)
 }
 
 pub const GRID_SIZE: (f32, f32) = (1000.0, 500.0);
 
+/// Methods responsible for main
+/// interactions with tree
 impl Node {
+    /// Calculates position of all terminal elements([Node::Category])
+    /// of the tree.
     pub fn layout(&mut self, pos: (f32, f32), size: (f32, f32)) {
         match self {
             Node::Category(c) => {
@@ -47,6 +60,9 @@ impl Node {
         }
     }
 
+    /// Turns tree into flat [Vec]<[Category]>.
+    /// 
+    /// ⚠️ **Does not calls **[Self::layout]** inside its body!**
     pub fn into_flat(self) -> Vec<Category> {
         match self {
             Node::Category(c) => {
@@ -57,11 +73,161 @@ impl Node {
             }
         }
     }
+}
 
+/// Methods responsible for navigations
+/// on the tree.
+impl Node {
+    /// Returns an `Vec` slice with child nodes
+    /// if there are some.
+    pub fn children(&self) -> Option<&[Node]> {
+        match self {
+            Node::Split(v, _) => if v.is_empty() { Some(v) } else { None },
+            Node::Category(_) => None,
+        }
+    }
+
+    /// Returns an mutable `Vec` slice with child nodes
+    /// if there are some.
+    pub fn children_mut(&mut self) -> Option<&mut [Node]> {
+        match self {
+            Node::Split(v, _) => Some(v),
+            Node::Category(_) => None,
+        }
+    }
+
+    /// Used to define if this node is terminal.
+    pub fn is_leaf(&self) -> bool {
+        if let Node::Category(_) = self {
+            true
+        } else { false }
+    }
+}
+
+/// Methods used to interact with nodes containment.
+impl Node {
+    /// Method used to append new node into container.
     pub fn push(&mut self, node: Node) -> Result<(), &'static str> {
         match self {
             Node::Category(_) => Err("Tried to push a node into a category"),
             Node::Split(v, _) => { v.push(node); Ok(()) }
+        }
+    }
+
+    /// Method used to insert new node into container
+    /// in a defined place.
+    pub fn insert(&mut self, node: Node, idx: usize) -> Result<(), &str> {
+        match self {
+            Node::Category(_) => Err("Tried to insert node into a category"),
+            Node::Split(v, _) => {
+                let max_idx = v.len() - 1;
+                if max_idx < idx {
+                    Err("Index is bigger than vector")
+                } else {
+                    v.insert(idx, node);
+                    Ok(())
+                }
+            }
+        }
+    }
+
+    /// Method used to remove a determined node
+    /// from the container.
+    pub fn remove(&mut self, idx: usize) -> Result<(), &str> {
+        match self {
+            Node::Category(_) => Err("Tried to remove node from a category"),
+            Node::Split(v, _) => {
+                let max_idx = v.len() - 1;
+                if max_idx < idx {
+                    Err("Index is bigger than vector")
+                } else {
+                    v.remove(idx);
+                    Ok(())
+                }
+            }
+        }
+    }
+
+    /// Method used to swap nodes
+    /// in the container.
+    /// 
+    /// ### Arguments
+    /// a: index 1
+    /// 
+    /// b: index 2
+    pub fn swap(&mut self, a: usize, b: usize) -> Result<(), &str> {
+        match self {
+            Node::Category(_) => Err("Tried to swap elements in category"),
+            Node::Split(v, _) => {
+                                let max_idx = v.len() - 1;
+                if max_idx < a || max_idx < b {
+                    Err("Index is bigger than vector")
+                } else if a == b {
+                    Ok(())
+                } else {
+                    v.swap(a, b);
+                    Ok(())
+                }
+            }
+        }
+    }
+
+    /// Method used to determine
+    /// number of nodes
+    /// in the container.
+    pub fn len(&self) -> Result<usize, &str> {
+        match self {
+            Node::Category(_) => Err("Tried to check length of a category"),
+            Node::Split(v, _) => {
+                Ok(v.len())
+            }
+        }
+    }
+
+    /// Method used to determine
+    /// if container is empty.
+    pub fn is_empty(&self) -> Result<bool, &str> {
+        match self {
+            Node::Category(_) => Err("Category cannot be empty"),
+            Node::Split(v, _) => Ok(v.is_empty())
+        }
+    }
+}
+
+impl Node {
+    /// Method used to get a reference to the
+    /// category with given name.
+    pub fn find(&self, name: &str) -> Option<&Node> {
+        match self {
+            Node::Category(c) => {
+                if c.name() == name { return Some(self) } else { None }
+            },
+            Node::Split(v, _) => {
+                for i in v.iter() {
+                    if let Some(n) = i.find(name) {
+                        return Some(n);
+                    }
+                }
+                None
+            }
+        }
+    }
+
+    /// Method used to get a **mutable** reference to the
+    /// category with given name.
+    pub fn find_mut(&mut self, name: &str) -> Option<&mut Node> {
+        match self {
+            Node::Category(c) => {
+                if c.name() == name { return Some(self) } else { None }
+            },
+            Node::Split(v, _) => {
+                for i in v.iter_mut() {
+                    if let Some(n) = i.find_mut(name) {
+                        return Some(n);
+                    }
+                }
+                None
+            }
         }
     }
 }
