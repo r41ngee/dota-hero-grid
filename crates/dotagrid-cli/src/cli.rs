90b0e52dc4fe1f0a8ea398d0918fd38434531b6e
r41ngee <r41ngee@yandex.ru>
2026-08-02 08:11:16 +0300

    feat: refactor CLI structure to use subcommands for actions

--- crates/dotagrid-cli/src/cli.rs
+++ crates/dotagrid-cli/src/cli.rs
@@ -1,8 +1,17 @@
 #[derive(clap::Parser)]
 pub struct Cli {
-    #[arg(help = "TOML config file path")]
-    pub input: String,
-
     #[arg(short, help = "output file")]
     pub output: Option<String>,
+
+    #[command(subcommand)]
+    pub action: Action,
+}
+
+#[derive(clap::Subcommand)]
+pub enum Action {
+    #[command()]
+    Build {
+        #[arg(help = "input file")]
+        input: String,
+    }
 }
--- crates/dotagrid-cli/src/main.rs
+++ crates/dotagrid-cli/src/main.rs
@@ -8,17 +8,22 @@ mod types;
 fn main() -> Result<(), anyhow::Error> {
     let args = cli::Cli::parse();
 
-    let filepath = args.input;
-    let file = fs::read_to_string(&filepath)?;
+    match &args.action {
+        cli::Action::Build { input } => {
+            let file = fs::read_to_string(input)?;
 
-    let sgm: types::SGridMap = toml::from_str(&file)?;
-    let gm: GridMap = sgm.into();
+            let sgm: types::SGridMap = toml::from_str(&file)?;
+            let gm: GridMap = sgm.into();
 
-    let json_gm = serialize_pretty(&gm)?;
+            let json_gm = serialize_pretty(&gm)?;
 
-    let output_path = args.output.unwrap_or_else(|| "hero_grid_config.json".to_string());
-    let mut ofile = fs::File::create(&output_path)?;
-    ofile.write_all(json_gm.as_bytes())?;
+            let output_path = args.output.unwrap_or_else(|| "hero_grid_config.json".to_string());
+            let mut ofile = fs::File::create(&output_path)?;
+            ofile.write_all(json_gm.as_bytes())?;
+        },
+    }
+
+
 
     Ok(())
 }
